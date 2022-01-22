e6503d4933c3d80aa02e428f572eb4d75004ad6a
muhammadabdumalikov <burakali424@gmail.com>
2022-01-22 05:32:07 +0500

    add CustomDrawer

--- src/navigation/DrawerNavigator.jsx
+++ src/navigation/DrawerNavigator.jsx
@@ -1,19 +1,22 @@
-
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import UserProfileScreen from "../screens/UserProfileScreen";
 import HomeScreen from "../screens/HomeScreen";
+import CustomDrawer from "../components/CustomDrawer";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="UserProfile" component={UserProfileScreen} />
-    </Drawer.Navigator>
-  );
-}
+    return (
+        <Drawer.Navigator
+            drawerContent={(props) => <CustomDrawer {...props} />}
+            screenOptions={{ headerShown: false }}
+        >
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="UserProfile" component={UserProfileScreen} />
+        </Drawer.Navigator>
+    );
+};
 
 export default DrawerNavigator;
