233465cd5893e33f4a9678015fc62f9b3a1dd790
muhammadabdumalikov <burakali424@gmail.com>
2022-01-22 06:14:20 +0500

    Add drawer items

--- src/components/CustomDrawer.jsx
+++ src/components/CustomDrawer.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { View, Text } from "react-native";
+import {
+    DrawerContentScrollView,
+    DrawerItemList,
+} from "@react-navigation/drawer";
 
 const CustomDrawer = (props) => {
     return (
-        <View>
-            <Text></Text>
+        <View style={{ flex: 1 }}>
+            <DrawerContentScrollView>
+                <View style={{ flex: 1, paddingTop: 10 }}>
+                    <DrawerItemList {...props} />
+                </View>
+                <View
+                    style={{
+                        borderTopWidth: 1,
+                        borderColor: "#ccc",
+                        marginVertical: 20,
+                        marginHorizontal: 16,
+                    }}
+                />
+                <Text
+                    style={{ marginLeft: 16, marginBottom: 10, fontSize: 17 }}
+                >
+                    Bog'lanish:
+                </Text>
+                <Text
+                    style={{ marginLeft: 16, fontSize: 19 }}
+                >
+                    +998910000000
+                </Text>
+            </DrawerContentScrollView>
+            <View>
+                <Text>Custom text</Text>
+            </View>
         </View>
     );
 };
--- src/navigation/DrawerNavigator.jsx
+++ src/navigation/DrawerNavigator.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 import UserProfileScreen from "../screens/UserProfileScreen";
 import HomeScreen from "../screens/HomeScreen";
 import CustomDrawer from "../components/CustomDrawer";
+import { colors } from "../constants/colors";
 
 const Drawer = createDrawerNavigator();
 
@@ -11,10 +13,44 @@ const DrawerNavigator = () => {
     return (
         <Drawer.Navigator
             drawerContent={(props) => <CustomDrawer {...props} />}
-            screenOptions={{ headerShown: false }}
+            screenOptions={{
+                headerShown: false,
+                drawerLabelStyle: {
+                    marginLeft: -20,
+                    fontSize: 17,
+                    color: "black",
+                },
+                // drawerActiveBackgroundColor: "white",
+                // drawerInactiveTintColor: colors.brandColor,
+                // drawerActiveTintColor: "white"
+            }}
         >
-            <Drawer.Screen name="Home" component={HomeScreen} />
-            <Drawer.Screen name="UserProfile" component={UserProfileScreen} />
+            <Drawer.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    drawerIcon: ({ color }) => (
+                        <Ionicons
+                            name="md-person-outline"
+                            size={26}
+                            color="black"
+                        />
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="UserProfile"
+                component={UserProfileScreen}
+                options={{
+                    drawerIcon: () => (
+                        <Ionicons
+                            name="md-information-circle-outline"
+                            size={26}
+                            color="black"
+                        />
+                    ),
+                }}
+            />
         </Drawer.Navigator>
     );
 };
