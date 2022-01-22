9dc6d477975ebb1f7cacbe5dfa65473f81eee3c9
muhammadabdumalikov <burakali424@gmail.com>
2022-01-23 02:30:49 +0500

    View change Pressable CardComponent

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet, Animated, Dimensions } from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Animated,
+    Dimensions,
+    Pressable,
+} from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { colors } from "../constants/colors";
 
@@ -41,44 +48,50 @@ const CardComponent = ({ index, item, y }) => {
     return (
         <Animated.View
             style={[
-                styles.box,
+                // styles.box,
                 { opacity, transform: [{ translateY }, { scale }] },
             ]}
             key={index}
         >
-            <View style={styles.nameLine}>
-                <Feather name="user" size={26} color={colors.brandColor} />
-                <Text
-                    style={styles.nameTxt}
-                >{`${item.firstName} ${item.lastName}`}</Text>
-            </View>
-
-            <View style={styles.secondLine}>
-                <View>
-                    <Text style={styles.preTxt}>Tug'ilgan yili</Text>
-                    <Text style={styles.secondLineTxt}>{`${item.bDate}`}</Text>
-                </View>
-                <View
-                    style={{
-                        height: "100%",
-                        borderLeftColor: "gray",
-                        borderLeftWidth: 0.5,
-                    }}
-                />
-                <View>
-                    <Text style={styles.preTxt}>Ta'minot turi</Text>
-                    <Text style={styles.secondLineTxt}>{`${item.type}`}</Text>
-                </View>
-            </View>
-
-            <View style={styles.thirddLine}>
-                <View>
-                    <Text style={styles.preTxt}>Summa</Text>
+            <Pressable style={styles.box}>
+                <View style={styles.nameLine}>
+                    <Feather name="user" size={26} color={colors.brandColor} />
                     <Text
-                        style={styles.thirddLineTxt}
-                    >{`${item.summ} so'm`}</Text>
+                        style={styles.nameTxt}
+                    >{`${item.firstName} ${item.lastName}`}</Text>
                 </View>
-            </View>
+
+                <View style={styles.secondLine}>
+                    <View>
+                        <Text style={styles.preTxt}>Tug'ilgan yili</Text>
+                        <Text
+                            style={styles.secondLineTxt}
+                        >{`${item.bDate}`}</Text>
+                    </View>
+                    <View
+                        style={{
+                            height: "100%",
+                            borderLeftColor: "gray",
+                            borderLeftWidth: 0.5,
+                        }}
+                    />
+                    <View>
+                        <Text style={styles.preTxt}>Ta'minot turi</Text>
+                        <Text
+                            style={styles.secondLineTxt}
+                        >{`${item.type}`}</Text>
+                    </View>
+                </View>
+
+                <View style={styles.thirddLine}>
+                    <View>
+                        <Text style={styles.preTxt}>Summa</Text>
+                        <Text
+                            style={styles.thirddLineTxt}
+                        >{`${item.summ} so'm`}</Text>
+                    </View>
+                </View>
+            </Pressable>
         </Animated.View>
     );
 };
